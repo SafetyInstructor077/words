5aa0d30e863d806784244310267f016715c6ab8e
Satan <64387276+SafetyInstructor077@users.noreply.github.com>
2024-03-26 15:03:42 -0400

    Interface v0.12

--- static/app.js
+++ static/app.js
@@ -25,7 +25,7 @@ menu.addEventListener('click', mobileMenu, navbarblack);
 //  Close mobile Menu when clicking on a menu item
 const hideMobileMenu = () => {
   const menuBars = document.querySelector('.is-active');
-  if (window.innerWidth <= 768 && menuBars) {
+  if (window.innerWidth >= 0 && menuBars) {
     menu.classList.toggle('is-active');
     menuLinks.classList.remove('active');
   }
