3de78255c72c95ef1175fdbc7f282d101af697a3
C <93102910+12ck12@users.noreply.github.com>
2024-04-02 11:06:44 -0400

    Update database.py

--- database.py
+++ database.py
@@ -3,6 +3,6 @@ from requests import get
 import re
 
 def add_account(account):
-    requete = f"""insert into Account (id, username, name, password) values (?, ?, ?, ?)"""
+    requete = f"""insert into Accounts (id, username, name, password) values (?, ?, ?, ?)"""
     id = get_max_id_nom()+1
     return _select(requete, (id, username, name, password))
