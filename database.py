7af731d25edcf3a806b55321d0c2d076f089aac1
C <93102910+12ck12@users.noreply.github.com>
2024-04-02 10:57:12 -0400

    Update database.py

--- database.py
+++ database.py
@@ -1,3 +1,8 @@
 import sqlite3
 from requests import get
 import re
+
+def add_account(account):
+    requete = f"""insert into Account (id, username, name, password) values (?, ?, ?, ?)"""
+    id = get_max_id_nom()+1
+    return _select(requete, (id, username, name, password))
