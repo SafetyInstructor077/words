9049ac5d9244b41e5711acf50b9ae6abaa8e825d
C <93102910+12ck12@users.noreply.github.com>
2024-04-16 11:52:09 -0400

    w

---
+++ static/journal.js
@@ -0,0 +1,33 @@
+function journal(event) {
+    event.preventDefault();
+
+    const form = document.forms["journal__right"]
+    let mess = form["journal__input"].value
+    let stat= form["stat"].value
+
+    fetch("/insert", {
+        method: "POST",
+        body: JSON.stringify({
+            "message": mess,
+            "status": stat,
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+    setTimeout(function () {
+        location.reload();
+    }, 6000);
+}
