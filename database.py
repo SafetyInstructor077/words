11e11f28811e9716060791d7d008e22fdbab61cb
Satan <64387276+SafetyInstructor077@users.noreply.github.com>
2024-05-06 16:03:17 -0400

    wordle

--- database.py
+++ database.py
@@ -82,4 +82,25 @@ def login(username, password):
             return False
     else:
         print("wrong user")
-        return False 
+        return False
+    
+
+# import csv
+# import json
+
+# #Read CSV file and convert it to a dictionary
+# csv_file = "sgb-words.csv"
+# json_data = []
+
+# with open(csv_file, 'r') as file:
+#     csv_reader = csv.reader(file)
+#     for row in csv_reader:
+#         json_data.append(row[1])
+
+# # Write JSON data to a file
+# json_file = "output.json"
+# with open(json_file, 'w') as file:
+#     json.dump(json_data, file)
+
+# print("Conversion complete. JSON file created: ", json_file)
+
